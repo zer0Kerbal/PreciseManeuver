768138707210aee246ae8f9653565bded5ca30fe
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-03-12 15:05:52 -0600

    rename folder

---
+++ source/Properties/AssemblyInfo.Unity.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PreciseManeuver.Unity")]
+[assembly: AssemblyProduct("PreciseManeuver.Unity")]
+[assembly: AssemblyCopyright("Copyleft (ɔ) 2017. Modify and distribute it at will! Hail open source!")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3ebb00a5-1bbe-46c1-ae28-01b5f6c603f6")]
+[assembly: AssemblyVersion("2.4.4.0")]
+[assembly: AssemblyFileVersion("2.4.4.0")]
