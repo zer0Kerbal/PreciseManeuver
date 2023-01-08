22dd58647127fafe7e17c67f2c76b7d57297694f
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2023-01-07 23:30:37 -0600

    Update AssemblyInfo.Unity.cs

--- source/Properties/AssemblyInfo.Unity.cs
+++ source/Properties/AssemblyInfo.Unity.cs
@@ -3,8 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("PreciseManeuver.Unity")]
 [assembly: AssemblyProduct("PreciseManeuver.Unity")]
-[assembly: AssemblyCopyright("Copyleft (ɔ) 2017. Modify and distribute it at will! Hail open source!")]
-[assembly: ComVisible(false)]
+[assembly: AssemblyCopyright("Copyleft (ɔ) 2017-2022. Modify and distribute it at will! Hail open source!")][assembly: ComVisible(false)]
 [assembly: Guid("3ebb00a5-1bbe-46c1-ae28-01b5f6c603f6")]
-[assembly: AssemblyVersion("2.4.4.0")]
-[assembly: AssemblyFileVersion("2.4.4.0")]
+[assembly: AssemblyVersion("2.4.99.1")]
+[assembly: AssemblyFileVersion("2.4.99.1")]
